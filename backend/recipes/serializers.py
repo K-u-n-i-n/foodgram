f8ae949fbd38321aaaf685fd8043512455faca6f
Alexander Kunin <K.u.n.i.n@yandex.ru>
2024-09-02 17:20:33 +0300

    Написать код для post и patch метода.

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from users.serializers import UserSerializer
+from tags.models import Tag
 from core.serializers import Base64ImageField
 from ingredients.models import Ingredient
 from tags.serializers import TagSerializer
@@ -29,9 +31,81 @@ class RecipeSerializer(serializers.ModelSerializer):
     ingredients = IngredientInRecipeSerializer(
         source='ingredientinrecipe_set', many=True
     )
-    tags = TagSerializer(read_only=True, many=True)
+    tags = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.all(), many=True)
 
     class Meta:
         model = Recipe
         fields = '__all__'
         read_only_fields = ('author',)
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['tags'] = TagSerializer(
+            instance.tags.all(), many=True).data
+        representation['author'] = UserSerializer(
+            instance.author).data
+
+        return representation
+
+    def create(self, validated_data):
+
+        tags = validated_data.pop('tags')
+        ingredients_data = validated_data.pop('ingredientinrecipe_set')
+
+        recipe = Recipe.objects.create(**validated_data)
+
+        for ingredient_data in ingredients_data:
+            IngredientInRecipe.objects.create(
+                recipe=recipe,
+                ingredient=ingredient_data['ingredient']['id'],
+                amount=ingredient_data['amount']
+            )
+
+        recipe.tags.set(tags)
+
+        return recipe
+
+    def update(self, instance, validated_data):
+        """Переопределение метода update."""
+
+        ingredients_data = validated_data.pop('ingredientinrecipe_set')
+        tags_data = validated_data.pop('tags', None)
+
+        instance.name = validated_data.get('name', instance.name)
+        instance.cooking_time = validated_data.get(
+            'cooking_time', instance.cooking_time)
+        instance.text = validated_data.get('text', instance.text)
+        instance.save()
+
+        if tags_data is not None:
+            instance.tags.set(tags_data)
+
+        if ingredients_data is not None:
+            new_ingredients = []
+
+            for ingredient_data in ingredients_data:
+                ingredient_obj = ingredient_data.get('ingredient')
+                ingredient_instance = ingredient_obj.get(
+                    'id') if ingredient_obj else None
+                ingredient_id = (
+                    ingredient_instance.id if ingredient_instance else None
+                )
+                amount = ingredient_data.get('amount')
+
+                if ingredient_id is not None:
+                    new_ingredients.append((ingredient_id, amount))
+
+            if new_ingredients:
+                instance.ingredients.clear()
+
+                for ingredient_id, amount in new_ingredients:
+                    IngredientInRecipe.objects.create(
+                        recipe=instance,
+                        ingredient_id=ingredient_id,
+                        amount=amount
+                    )
+
+        return instance
+    
+#  Получить короткую ссылку на рецепт
--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -9,6 +9,7 @@ class RecipeViewSet(ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     permission_classes = [IsAuthenticated]
+    # http_method_names = ['get', 'post', 'patch', 'delete']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
