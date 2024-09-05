206064c6eb0c4e7dd963db0cff9296612ca64682
Alexander Kunin <K.u.n.i.n@yandex.ru>
2024-09-05 16:40:49 +0300

    Написать декораторы: subscribe, subscriptions, get_link, resolve_link, download_shopping_cart

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,20 @@
+from collections import defaultdict
+import csv
+from io import StringIO
+import os
+
+import hashids
+from django.http import HttpResponse
 from django.contrib.auth import get_user_model
 from django_filters.rest_framework import DjangoFilterBackend
+from dotenv import load_dotenv, find_dotenv
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.exceptions import ValidationError
-from rest_framework.generics import ListAPIView
-from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.exceptions import ValidationError, ParseError
+from rest_framework.permissions import (AllowAny,
+                                        IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly
+                                        )
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -24,14 +34,18 @@ from .serializers import (
     FavoriteSerializer,
     IngredientSerializer,
     RecipeSerializer,
-    SubscriptionSerializer,
     TagSerializer,
     UserRegistrationSerializer,
-    UserSerializer
+    UserSerializer,
+    UserSubscriptionSerializer
 )
 
 
 User = get_user_model()
+
+load_dotenv(find_dotenv())
+
+BASE_URL = os.getenv('BASE_URL', 'http://127.0.0.1:8000')
 
 
 class UserViewSet(ModelViewSet):
@@ -43,6 +57,76 @@ class UserViewSet(ModelViewSet):
         if self.action == 'create':
             return UserRegistrationSerializer
         return super().get_serializer_class()
+
+    @action(
+        detail=True,
+        methods=['post'],
+        permission_classes=[IsAuthenticated]
+    )
+    def subscribe(self, request, pk=None):
+        user = self.get_object()
+        current_user = request.user
+
+        if user == current_user:
+            return Response(
+                {"detail": "Нельзя подписаться на самого себя."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if Subscription.objects.filter(
+                user=current_user, author=user).exists():
+            return Response(
+                {"detail": "Вы уже подписаны на этого пользователя."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        Subscription.objects.create(user=current_user, author=user)
+
+        recipes_limit = request.query_params.get('recipes_limit')
+        if recipes_limit is not None:
+            try:
+                recipes_limit = int(recipes_limit)
+            except ValueError:
+                raise ParseError("Параметр recipes_limit должен быть числом.")
+
+        context = self.get_serializer_context()
+        context['recipes_limit'] = recipes_limit
+
+        user_data = UserSubscriptionSerializer(user, context=context).data
+        return Response(user_data, status=status.HTTP_201_CREATED)
+
+    @action(
+        detail=False,
+        methods=['get'],
+        url_path='subscriptions'
+    )
+    def subscriptions(self, request):
+        user = request.user
+
+        recipes_limit = request.query_params.get('recipes_limit')
+        if recipes_limit is not None:
+            try:
+                recipes_limit = int(recipes_limit)
+            except ValueError:
+                raise ParseError("Параметр recipes_limit должен быть числом.")
+
+        subscriptions = Subscription.objects.filter(
+            user=user).select_related('author')
+        authors = [subscription.author for subscription in subscriptions]
+
+        page = self.paginate_queryset(authors)
+        if page is not None:
+            serializer = UserSubscriptionSerializer(
+                page, many=True,
+                context={'request': request, 'recipes_limit': recipes_limit}
+            )
+            return self.get_paginated_response(serializer.data)
+
+        serializer = UserSubscriptionSerializer(
+            authors, many=True,
+            context={'request': request, 'recipes_limit': recipes_limit}
+        )
+        return Response(serializer.data)
 
 
 class UserSelfView(APIView):
@@ -84,26 +168,11 @@ class UserAvatarView(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class SubscriptionListView(ListAPIView):
-    serializer_class = SubscriptionSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        return Subscription.objects.filter(user=self.request.user)
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-
-
 class TagViewSet(ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     http_method_names = ['get']
     pagination_class = None
-
-    # Разобраться с поиском и фильтрацией
 
 
 class RecipeViewSet(ModelViewSet):
@@ -117,18 +186,20 @@ class RecipeViewSet(ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    def partial_update(self, request, *args, **kwargs):
-        required_fields = [
-            'ingredients', 'tags', 'name', 'text', 'cooking_time'
-        ]
-        for field in required_fields:
-            if field not in request.data:
-                return Response(
-                    {field: 'Это поле является обязательным.'},
-                    status=status.HTTP_400_BAD_REQUEST
-                )
+    def update(self, request, *args, **kwargs):
 
-        return super().partial_update(request, *args, **kwargs)
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+
+        serializer = self.get_serializer(
+            instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    # Избыточен, добавление в закладки
+    def partial_update(self, request, *args, **kwargs):
+        return self.update(request, *args, partial=True, **kwargs)
 
     @action(
         detail=True,
@@ -168,6 +239,72 @@ class RecipeViewSet(ModelViewSet):
             recipe, context={'request': request}).data
 
         return Response(recipe_data, status=status.HTTP_201_CREATED)
+
+    @action(
+        detail=True,
+        methods=['get'],
+        url_path='get-link'
+    )
+    def get_link(self, request, pk=None):
+        recipe = self.get_object()
+        hashid = hashids.Hashids(salt="random_salt", min_length=8)
+        short_id = hashid.encode(recipe.id)
+        short_link = f'{BASE_URL}/s/{short_id}'
+        return Response({"short-link": short_link})
+
+    @action(
+        detail=False,
+        methods=['get'],
+        url_path='resolve-link/(?P<short_id>[^/.]+)'
+    )
+    def resolve_link(self, request, short_id):
+        hashid = hashids.Hashids(salt="random_salt", min_length=8)
+        decoded = hashid.decode(short_id)
+        if not decoded:
+            return Response({"error": "Invalid short link"}, status=400)
+
+        recipe_id = decoded[0]
+        recipe = Recipe.objects.filter(id=recipe_id).first()
+        if not recipe:
+            return Response({"error": "Recipe not found"}, status=404)
+        return Response(
+            {"id": recipe.id,
+             "name": recipe.name,
+             "description": recipe.description}
+        )
+
+    @action(
+        detail=False,
+        methods=['get'],
+        permission_classes=[IsAuthenticated]
+    )
+    def download_shopping_cart(self, request):
+        user = request.user
+        shopping_cart_items = ShoppingCart.objects.filter(user=user)
+        ingredient_totals = defaultdict(lambda: {'amount': 0, 'unit': ''})
+
+        for item in shopping_cart_items:
+            recipe = item.recipe
+            for ri in recipe.ingredientinrecipe_set.all():
+                ingredient_name = ri.ingredient.name
+                unit = ri.ingredient.measurement_unit
+                ingredient_totals[ingredient_name]['amount'] += ri.amount
+                ingredient_totals[ingredient_name]['unit'] = unit
+
+        output = StringIO()
+        writer = csv.writer(output)
+        writer.writerow(['Ингредиенты', 'Количество'])
+
+        for ingredient, data in ingredient_totals.items():
+            amount = data['amount']
+            unit = data['unit']
+            writer.writerow([f'{ingredient} ({unit})', f'{amount}'])
+
+        response = HttpResponse(output.getvalue(), content_type='text/csv')
+        response['Content-Disposition'] = (
+            'attachment; filename="shopping_cart.csv"'
+        )
+        return response
 
 
 class IngredientViewSet(ModelViewSet):
