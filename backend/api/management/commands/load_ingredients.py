c2d7a26d13314a120c7f7e9dcb1ea9dad09844fd
Alexander Kunin <K.u.n.i.n@yandex.ru>
2024-09-05 22:04:23 +0300

    Написать код для заполнения БД ингредиентами.

---
+++ backend/api/management/commands/load_ingredients.py
@@ -0,0 +1,36 @@
+import csv
+import os
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    help = 'Загрузка ингредиентов в базу данных из CSV-файла'
+
+    def handle(self, *args, **kwargs):
+        file_path = os.path.join(
+            settings.BASE_DIR, '..', 'data', 'ingredients.csv'
+        )
+
+        try:
+            with open(file_path, mode='r', encoding='utf-8') as file:
+                reader = csv.reader(file)
+                count = 0
+                for row in reader:
+                    name, measurement_unit = row
+                    Ingredient.objects.get_or_create(
+                        name=name, measurement_unit=measurement_unit
+                    )
+                    count += 1
+                    if count % 100 == 0:
+                        self.stdout.write(self.style.SUCCESS(
+                            f'{count} записей обработано ...'))
+            self.stdout.write(self.style.SUCCESS(
+                'Ингредиенты успешно загружены'))
+        except FileNotFoundError:
+            self.stdout.write(self.style.ERROR(f'Файл не найден: {file_path}'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'Произошла ошибка: {e}'))
