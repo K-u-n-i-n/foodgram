9acd3d25ccf8fdada18387c9996fe45f25d784ea
Alexander Kunin <K.u.n.i.n@yandex.ru>
2024-09-10 15:06:22 +0300

    Поправить ALLOWED_HOSTS.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['BASE_URL', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [os.getenv('BASE_URL'), 'localhost', '127.0.0.1']
 
 
 # Application definition
