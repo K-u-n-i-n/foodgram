6917260b0aec42559f340712e91cf14678ecb93e
Alexander Kunin <K.u.n.i.n@yandex.ru>
2024-09-12 15:16:19 +0300

    Вынести кастомное поле Base64ImageField в fields.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,12 +1,8 @@
-import base64
-import imghdr
-import uuid
-
 from django.contrib.auth import get_user_model
-from django.core.files.base import ContentFile
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
+from api.fields import Base64ImageField
 from api.mixins import IsSubscribedMixin
 from recipes.models import (Favorite,
                             Ingredient,
@@ -15,28 +11,6 @@ from recipes.models import (Favorite,
                             ShoppingCart,
                             Tag
                             )
-
-
-class Base64ImageField(serializers.ImageField):
-    """
-    Поле сериализатора для обработки изображений,
-    закодированных в формате Base64.
-    """
-
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-            img_data = base64.b64decode(imgstr)
-
-            file_name = f'{uuid.uuid4()}.{ext}'
-
-            if not imghdr.what(None, img_data):
-                raise serializers.ValidationError('Неверное изображение')
-
-            data = ContentFile(img_data, name=file_name)
-
-        return super().to_internal_value(data)
 
 
 User = get_user_model()
